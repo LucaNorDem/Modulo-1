f94e07df97fcffcb5e1e4fca28523d9f2694eafd
LucaNor <lucascythe@gmail.com>
2024-01-08 20:00:44 -0300

    Homework de TESTING

---
+++ EXTRA-Testing/homework/homework.js
@@ -0,0 +1,74 @@
+function checkSeatStatus(row, number){
+    if(typeof row !== 'string') throw new TypeError('First parameter is not a string');
+    if(typeof number !== 'number') throw new TypeError('Second parameter is not a number');
+
+    
+    const seat = getSeat(row, number);
+    return seat.booked;
+}
+
+
+function getRowNumber(letter){
+    return letter.charCodeAt(0) - 65;
+    
+}
+
+function book(row, number){
+    const bookedSeat = checkSeatStatus(row,number);
+    const rowNumber = getRowNumber(row);
+    if(bookedSeat){
+        return "Seat in " + row + number + " is already booked";
+    }else{
+        layout[rowNumber][number].booked = true;
+        return "Seat in " + row + number + " succesfully booked";
+    }
+}
+
+function getSeat(letter, number){
+    const numberRow = getRowNumber(letter);
+    const layoutRows = layout[numberRow];
+    const seat = layoutRows[number];
+    return seat;
+}
+
+const layout = [
+    [
+       { type: 'VIP', booked: false },
+       { type: 'VIP', booked: true },
+       { type: 'VIP', booked: true },
+       { type: 'VIP', booked: false },
+    ],
+    [
+       { type: 'NORMAL', booked: false },
+       { type: 'VIP', booked: true },
+       { type: 'VIP', booked: false },
+       { type: 'NORMAL', booked: false },
+    ],
+    [
+       { type: 'NORMAL', booked: false },
+       { type: 'NORMAL', booked: true },
+       { type: 'NORMAL', booked: true },
+       { type: 'NORMAL', booked: false },
+    ],
+    [
+       { type: 'ECONOMIC', booked: true },
+       { type: 'NORMAL', booked: true },
+       { type: 'NORMAL', booked: true },
+       { type: 'ECONOMIC', booked: false },
+    ],
+    [
+       { type: 'ECONOMIC', booked: false },
+       { type: 'ECONOMIC', booked: true },
+       { type: 'ECONOMIC', booked: false },
+       { type: 'ECONOMIC', booked: false },
+    ],
+];
+
+
+
+
+module.exports = {
+    checkSeatStatus,
+    getRowNumber,
+    book
+}
---
+++ EXTRA-Testing/homework/tests/index.test.js
@@ -0,0 +1,49 @@
+const { checkSeatStatus, getRowNumber, book } = require('../homework');
+
+describe("checkSeatStatus",()=>{
+    it('checkSeatStatus is a function', () => { 
+        expect(typeof checkSeatStatus).toBe('function'); 
+    });
+
+    it("should throw TypeError if first parameter is not a string", ()=>{
+        expect(()=> checkSeatStatus(4)).toThrow(TypeError)
+    })
+    it("Should throw TypeErroe if second parameter is not a number", ()=>{
+        expect(()=> checkSeatStatus("A","2")).toThrow(TypeError)
+    })
+
+
+    it("Should return true if the given seat defined by row and column is booked", ()=>{
+        expect(checkSeatStatus('A', 1)).toBe(true);
+    })
+    it('should return false if the given seat defined by row and column is not booked', () => {
+        expect(checkSeatStatus('E', 3)).toBe(false); 
+    });
+})
+
+describe("getRowNumber", ()=>{
+    it("Should return 1 if the letter given is an A", ( )=>{
+        expect(getRowNumber("A")).toBe(0);
+    })
+    it("Should return 3 if the letter given is an C", ( )=>{
+        expect(getRowNumber("C")).toBe(2);
+    })
+})
+
+describe.only("book", ()=>{
+    it("Should return -Seat in E3 successfully booked- if the givin seat is not booked", ()=>{
+        expect(checkSeatStatus("E",3)).toBe(false);
+        expect(book("E", 3)).toBe("Seat in E3 succesfully booked");
+        expect(checkSeatStatus("E",3)).toBe(true);
+    })
+
+    it("Should return -Seat in A3 successfully booked- if the givin seat is not booked", ()=>{
+        expect(checkSeatStatus("A",3)).toBe(false);
+        expect(book("A", 3)).toBe("Seat in A3 succesfully booked");
+        expect(checkSeatStatus("A",3)).toBe(true);
+    })
+
+    it("Should return -Seat in A1 is already booked- if the givin seat is booked", ()=>{
+        expect(book("A", 1)).toBe("Seat in A1 is already booked");
+    })
+})
