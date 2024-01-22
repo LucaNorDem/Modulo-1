1d27874317f6345d6bcc6e61357e0fb0fd82a1a3
LucaNor <lucascythe@gmail.com>
2024-01-22 17:14:50 -0300

    Homework algoritmos 1 sin factorear

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -6,6 +6,8 @@ function factorear(num) {
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
+  var arr = [];
+  var i = 1;
 
 }
 
@@ -14,7 +16,21 @@ function bubbleSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  var orden = false;
 
+  while(!orden){
+    orden = true;
+    for(let i = 0; i < array.length -1; i++){
+      if(array[i] > array[i + 1]){
+        let aux = array[i];
+        array[i] = array[i+1];
+        array[i+1] = aux;
+        orden = false;
+      }
+    }
+  }
+
+  return array;
 }
 
 
@@ -23,6 +39,19 @@ function insertionSort(array) {
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
+  
+  for(let i = 1; i < array.length; i++){
+    if(array[i] < array[i-1]){
+      let aux = array[i];
+      while(aux < array[i-1]){
+        array[i] = array[i-1];
+        i--
+      }
+      array[i] = aux;
+    }
+  }
+
+  return array;
 
 }
 
@@ -32,7 +61,19 @@ function selectionSort(array) {
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
+  for(let i = 0; i < array.length - 1; i++){
+    let minIndex = i;
+    for(let j = i + 1; j < array.length; j++){
+      if(array[j] < array[minIndex]){
+        minIndex = j;
+      }
+    }
+    let aux = array[i];
+    array[i] = array[minIndex];
+    array[minIndex] = aux;
+  }
 
+  return array;
 }
 
 
