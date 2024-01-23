e6455f94a2787cba5ab01634118232696f0f5de4
LucaNor <lucascythe@gmail.com>
2024-01-23 17:24:49 -0300

    homework con arreglos y completada

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -6,9 +6,19 @@ function factorear(num) {
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
-  var arr = [];
-  var i = 1;
+  var arr = [1];
+  let divisor = 2
 
+  while(num > 1){
+    if(num % divisor === 0){
+      num = num / divisor;
+      arr.push(divisor);
+    }else{
+      divisor++;
+    }
+  }
+
+  return arr;
 }
 
 function bubbleSort(array) {
@@ -41,13 +51,14 @@ function insertionSort(array) {
   // Tu código:
   
   for(let i = 1; i < array.length; i++){
-    if(array[i] < array[i-1]){
-      let aux = array[i];
-      while(aux < array[i-1]){
-        array[i] = array[i-1];
-        i--
+    let j = i - 1;
+    let aux = array[i];
+    if(j >= 0 && aux < array[j]){
+      while(aux < array[j]){
+        array[j + 1] = array[j];
+        j--
       }
-      array[i] = aux;
+      array[j + 1] = aux;
     }
   }
 
